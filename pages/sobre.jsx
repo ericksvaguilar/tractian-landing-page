624d3bbcd7cb11af1d9cd4fae3c43e0779445beb
Erick Aguilar <ericksvaguilar@gmail.com>
2021-09-19 12:51:25 -0300

    adjust sobre page

--- pages/sobre.jsx
+++ pages/sobre.jsx
@@ -1,7 +1,11 @@
+import styles from '../styles/Sobre.module.scss'
+
 export default function Sobre() {
   return (
-    <div>
-      <h1>Sobre</h1>
+    <div className={styles.Sobre}>
+      <section>
+        <h1>Sobre</h1>
+      </section>
     </div>
   )
 }
